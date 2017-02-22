dda919446a56670630587c93a99671e6620ed70e
russellgoldenberg <russellgoldenberg@gmail.com>
2017-02-22 15:57:42 -0500

    merge in changes

--- src/responsive-table.js
+++ src/responsive-table.js
@@ -116,7 +116,6 @@
 	const createHTML = ({ editable }) => {
 		return `
 <style>
-	@import url('https://fonts.googleapis.com/css?family=Lato:400,900');
 	@@include("../.tmp/table-style.css")
 </style>
 <div class='rg-container'>
